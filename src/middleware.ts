053a54879a1641b35d66c54cb21accf27014b2a5
Alok <alok@como.com>
2024-01-20 14:58:49 +0400

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import { auth } from "../auth";
+
+export default auth((req) => {
+  console.log("ROUTE", req.nextUrl.pathname);
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
