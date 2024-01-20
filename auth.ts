a88e9d7ddcb91f73f9811fd603a360368034557c
Alok <alok@como.com>
2024-01-20 15:34:01 +0400

    added correct bcrypt

--- auth.ts
+++ auth.ts
@@ -1,9 +1,39 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "@/lib/db";
+import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
+import { getAccountByUserId } from "./data/account";
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 } = NextAuth({
-  providers: [GitHub],
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
+  callbacks: {
+    async signIn({ user, account }) {},
+    async session({ token, session }) {
+      return "session";
+    },
+    async jwt({ token }) {
+      return "token";
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
