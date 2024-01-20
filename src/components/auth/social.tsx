99b4ce39d3a9d7f49039d6241fafa958711b7258
Alok <alok@como.com>
2024-01-20 20:34:57 +0400

    added social login

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -6,15 +6,14 @@ import { FaGithub } from "react-icons/fa";
 import { useSearchParams } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "../../../routes";
 // import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
-    const searchParams = useSearchParams();
-    const callbackUrl = searchParams.get("callbackUrl");
 
     const onClick = (provider: "google" | "github") => {
         signIn(provider, {
-            callbackUrl: callbackUrl || 'DEFAULT_LOGIN_REDIRECT',
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
         });
     }
 
