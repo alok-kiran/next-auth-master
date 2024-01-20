ffa3b2ba9a3bf4aafdf435cc96ad1b687b13e734
Alok <alok@como.com>
2024-01-20 20:10:28 +0400

    added callbacks, jwt, session

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "@/lib/db";
@@ -6,6 +6,16 @@ import authConfig from "./auth.config";
 import { getUserById } from "./data/user";
 import { getAccountByUserId } from "./data/account";
 import { DEFAULT_LOGIN_REDIRECT, publicRoutes, authRoutes } from "./routes";
+import { JWT } from "next-auth/jwt";
+
+declare module "@auth/core" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      role: "ADMIN" | "USER";
+    } & DefaultSession["user"];
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -16,6 +26,25 @@ export const {
   pages: {
     signIn: "/auth/login",
     error: "/auth/error",
+  },
+  callbacks: {
+    //@ts-ignore
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const user = await getUserById(token.sub);
+      if (!user) return token;
+      token.role = user.role;
+      return token;
+    },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
